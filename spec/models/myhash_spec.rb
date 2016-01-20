f42e31d155f94b148e9067b8abf4d17e32194786
DimaAlex <dimaalekseichik@gmail.com>
2016-01-21 00:55:13 +0300

    added tests for #delete, #add_many, #values and #keys

--- spec/models/myhash_spec.rb
+++ spec/models/myhash_spec.rb
@@ -2,14 +2,29 @@ require '/home/dima/RubyTasks/MyHash.rb'
 
 describe MyHash do
   describe "#[key]" do
-    let(:hash) { MyHash.new }
+    let(:hash) { MyHash.new(1488) }
     before do
+      10.times { |i| hash[i] = i }
       hash['key'] = 'value' 
     end
 
     it "return 'value'" do
       expect(hash['key']).to eq 'value'
     end
+
+	it "return 'default' value" do
+      expect(hash[123]).to eq 1488
+	end   
+
+	context "when add many pairs key-value" do
+	  before do 
+	    hash.clear 
+	    hash.add_many(:a, 1, :b, 2, :c, 3, :a, 14, :a, 15, :b)
+	  end
+	  it "return 'hash' with values" do
+        expect(hash.to_s).to eq "{a => 15, b => 1488, c => 3, }"
+      end
+	end
   end
 
   describe "#length" do
@@ -18,7 +33,7 @@ describe MyHash do
       100.times { |i| hash[i] = i } 
     end
 
-    it "return length of MyHash" do
+    it "return length of 'hash'" do
       expect(hash.length).to eq 100
     end
   end
@@ -34,12 +49,42 @@ describe MyHash do
     end
 
     it "return 'true' if hashes are equal" do
-      expect(hash.eql?(otherhash)).should be_true
+      expect(hash.eql?(otherhash)).to eq true
+    end
+    
+    context "with change in our 'otherhash'" do
+      before { otherhash[50] = 'value' }
+
+      it "return 'false' if hashes are different" do
+        expect(hash.eql?(otherhash)).to eq false
+      end
+    end
+  end
+
+  describe "#delete" do
+    let(:hash) { MyHash.new }
+    before do
+      10.times { |i| hash[i] = i}
+      hash.delete(5)
     end
 
-    before { otherhash['key'] = 'value' }
-    it "return 'false' if hashes are different" do
-      expect(hash.eql?(otherhash)).should be_false
+    it "delete pair key-value" do
+      expect(hash.delete(5)).to eq 'There is no key in MyHash'
+    end
+  end
+
+  describe "#values and #keys" do
+    let(:hash) { MyHash.new }
+    before do
+      10.times { |i| hash[i] = i}
+    end
+
+    it "return array of values" do
+      expect(hash.values).to eq (0..9).to_a
+    end
+
+    it "return array of keys" do
+      expect(hash.keys).to eq (0..9).to_a
     end
   end
 end
