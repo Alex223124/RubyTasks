a09f1bcb6eb67c130d3e79b0714508b6c2e426d4
DimaAlex <dimaalekseichik@gmail.com>
2016-01-20 16:29:49 +0300

    added tests for #length and #eql?

--- spec/models/myhash_spec.rb
+++ spec/models/myhash_spec.rb
@@ -6,8 +6,40 @@ describe MyHash do
     before do
       hash['key'] = 'value' 
     end
+
     it "return 'value'" do
       expect(hash['key']).to eq 'value'
+    end
+  end
+
+  describe "#length" do
+    let(:hash) { MyHash.new }
+    before do
+      100.times { |i| hash[i] = i } 
+    end
+
+    it "return length of MyHash" do
+      expect(hash.length).to eq 100
+    end
+  end
+
+  describe "#eql?" do
+    let(:hash) { MyHash.new }
+    let(:otherhash) { MyHash.new }
+    before do
+      100.times do |i|
+        hash[i] = i
+        otherhash[99 - i] = 99 - i
+      end
+    end
+
+    it "return 'true' if hashes are equal" do
+      expect(hash.eql?(otherhash)).should be_true
+    end
+
+    before { otherhash['key'] = 'value' }
+    it "return 'false' if hashes are different" do
+      expect(hash.eql?(otherhash)).should be_false
     end
   end
 end
