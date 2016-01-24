eb3d115959e7e8a600b005b67e51e4be73039ba8
DimaAlex <dimaalekseichik@gmail.com>
2016-01-25 02:38:12 +0300

    add #add_many, #eql?, #to_s, change #clear,#empty and #[]=

--- MyHash/newMyHash.rb
+++ MyHash/newMyHash.rb
@@ -1,10 +1,9 @@
 Pair = Struct.new(:key, :value)
 
 class MyHash
-
   def initialize(default = nil)
     @main_array = []
-    @const = 50000
+    @const = 50_000
     @default = default
     @size = 0
   end
@@ -19,9 +18,10 @@ class MyHash
     @main_array[index] ||= []
     if @main_array[index].empty?
       @main_array[index] << pair
-      
     else
-      @main_array[index].each { |p| return p = pair if p.key == pair.key }
+      @main_array[index].each_with_index do |p, i|
+        return @main_array[index][i] = pair if p.key == pair.key
+      end
       @main_array[index] << pair
     end
     @size += 1
@@ -42,11 +42,12 @@ class MyHash
   end
 
   def clear
+    @size = 0
     @main_array.clear
   end
 
   def empty?
-    @main_array.empty? ? true : false
+    @main_array.empty?
   end
 
   def keys
@@ -65,11 +66,44 @@ class MyHash
     value_array.sort
   end
 
-  def to_s
+  def delete(key)
+    index = hash_func(key)
+    arr = @main_array[index]
+    if arr.nil? || arr.empty?
+      'There is no key in MyHash'
+    else
+      arr.each_index { |i| arr.delete_at(i) if arr[i].key == key }
+      @size -= 1
+    end
+  end
 
+  def to_s
+    out = []
+    @main_array.each do |arr|
+      str = ""
+      unless arr.nil? || arr.empty?
+        arr.each do |pair|
+          str << pair.key.inspect
+          str << '=>'
+          str << pair.value.inspect
+        end
+        out << str
+      end
+    end
+    "{#{out.join(', ')}}"
+  end
+
+  def eql?(other_hash)
+    return false unless other_hash.class == MyHash && self.length == other_hash.length
+
+    self.keys.each do |key|
+      return false unless other_hash.keys.include?(key) && self[key] == other_hash[key]
+    end
+    true
+  end
+
+  def add_many(*args)
+    return "please, enter an even number of arguments" if args.size.odd?
+    args.each_index { |i| self[args[i - 1]] = args[i] if i.odd? }
   end
 end
-
-#var = MyHash.new
-#100_000.times {|i| var[i] = i}
-#puts var.keys.size
