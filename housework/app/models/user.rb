27f66c7491d5d47b3d4f69117e3f887003b84121
DimaAlex <dimaalekseichik@gmail.com>
2016-02-05 00:30:03 +0300

    add omniauth-facebook but there is bug

--- housework/app/models/user.rb
+++ housework/app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
   has_many :tasks
 
   has_many :parent_relationships, :foreign_key => "parent_id", :class_name => "Relationship"
@@ -10,4 +12,22 @@ class User < ActiveRecord::Base
 
   has_many :child_relationships, :foreign_key => "child_id", :class_name => "Relationship"
   has_many :parents, :through => :child_relationships
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      #user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
+
