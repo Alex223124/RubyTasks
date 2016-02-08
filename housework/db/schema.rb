a8f9e087ec5d35208e03812a308c2a6e485c260e
DimaAlex <dimaalekseichik@gmail.com>
2016-02-08 13:47:35 +0300

    remove field age from User

--- housework/db/schema.rb
+++ housework/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207154452) do
+ActiveRecord::Schema.define(version: 20160208104618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,6 @@ ActiveRecord::Schema.define(version: 20160207154452) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.integer  "age"
     t.date     "birthday"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
